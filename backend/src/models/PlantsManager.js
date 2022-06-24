ad8e362d43a00ba1e8631bb9bcf6399a6f05b6a1
Attila Simon <simon.attila.fr@gmail.com>
2022-06-24 10:31:49 +0200

    Category and inner join has been added to get route of /plants

--- backend/src/models/PlantsManager.js
+++ backend/src/models/PlantsManager.js
@@ -3,6 +3,12 @@ const AbstractManager = require("./AbstractManager");
 class PlantsManager extends AbstractManager {
   static table = "plant";
 
+  findAll() {
+    return this.connection.query(
+      `select *, c.name as category from  ${this.table} inner join category as c on plant.category_id=c.id`
+    );
+  }
+
   insert(plant) {
     return this.connection.query(
       `insert into ${PlantsManager.table} (name, category_id, light, water, price, image) values (?, ?, ?, ?, ?, ?)`,
